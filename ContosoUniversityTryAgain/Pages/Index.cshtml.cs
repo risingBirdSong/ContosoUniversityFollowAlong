4fa42f54c65eaaee52293dfd3f4885cc803334ad
risingbirdsong <heregoingthere@outlook.com>
2019-10-14 15:57:26 -0700

    Committed all from first step of tutorial ... "Get Started" , everything working so far... note, instead of EnsureCreated I just went with database migrations straightaway instead

--- ContosoUniversityTryAgain/Pages/Index.cshtml.cs
+++ ContosoUniversityTryAgain/Pages/Index.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace ContosoUniversityTryAgain.Pages
+namespace ContosoUniversity.Pages
 {
     public class IndexModel : PageModel
     {
--- ContosoUniversityTryAgain/Startup.cs
+++ ContosoUniversityTryAgain/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,8 +8,10 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
 
-namespace ContosoUniversityTryAgain
+namespace ContosoUniversity
 {
     public class Startup
     {
@@ -24,6 +26,11 @@ namespace ContosoUniversityTryAgain
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddDbContext<SchoolContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SchoolContext")));
+
+         
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
